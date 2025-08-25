c97f296b35c3dec906ad9036742a8aa453537575
praveen858267 <chanti4ver@gmail.com>
2025-08-25 22:31:06 +0530

    Add files via upload

---
+++ dad.js
@@ -0,0 +1,9 @@
+let lokesh=document.getElementById('lokesh')
+// console.log(lokesh)
+// console.log(lokesh.textContent)
+
+function newbutton(){
+    lokesh.textContent='I have started learning java script'
+    lokesh.style.color="blue"
+    
+}
